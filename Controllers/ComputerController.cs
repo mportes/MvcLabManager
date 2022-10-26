aa0a1ddd62177f954f1e4daf6cdf0eb4b1aca22b
Matheus Portes <65133000+mportes@users.noreply.github.com>
2022-10-26 09:08:16 -0300

    Add files via upload

---
+++ Controllers/ComputerController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using MvcLabManager.Models;
+
+namespace MvcLabManager.Controllers;
+
+public class ComputerController : Controller
+{
+    private readonly LabManagerContext _context;
+
+    public ComputerController(LabManagerContext context) {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        return View(_context.Computers);
+    }
+
+    public IActionResult Show(int id)
+    {
+        Computer computer = _context.Computers.Find(id);
+
+        if(computer == null)
+        {
+            return NotFound();
+        }
+
+        return View(computer);
+    }
+
+    public IActionResult Delete(int id)
+    {
+        _context.Computers.Remove(_context.Computers.Find(id));
+        _context.SaveChanges();
+        return RedirectToAction("Index");
+    }
+}
